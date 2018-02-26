d36309a6f40549cce152d059331b03a0ad729ac3
fexd-labo <hiroki_usui@fexd.co.jp>
2018-02-26 17:19:37 +0900

    ダミーのテストモジュール

---
+++ test/dummy.exs
@@ -0,0 +1,10 @@
+defmodule CliTest do
+    use ExUnit.Case
+    doctest IssuesList
+
+    test "dummy test function" do
+        IO.puts("dummy test function")
+        assert 1 == 3
+    end
+
+end
