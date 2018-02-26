c55c0d46108708b3a7699115a9cb30fda1214dd3
fexd-labo <hiroki_usui@fexd.co.jp>
2018-02-26 17:30:17 +0900

    テストケース追加

--- test/dummy.exs
+++ test/dummy.exs
@@ -2,9 +2,18 @@ defmodule CliTest do
     use ExUnit.Case
     doctest IssuesList
 
-    test "dummy test function" do
-        IO.puts("dummy test function")
+    test "dummy test function No1" do
+        IO.puts("dummy test function No1")
         assert 1 == 3
     end
 
+    test "dummy test function No2" do
+        IO.puts("dummy test function No2")
+        assert 1 == 3
+    end
+
+    test "dummy test function No3" do
+        IO.puts("dummy test function No3")
+        assert 1 == 3
+    end
 end
